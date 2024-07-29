bf8639fc3515ae28cada72ef5f6ade1c7889d758
GabrielCerdeiraCandidoAlves <106674372+GabrielCerdeira@users.noreply.github.com>
2024-07-29 11:01:02 -0300

    pedro safado

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-
+// Adiciona evento de envio ao formulário
+document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    const nome = document.querySelector('input[name="nome"]').value;
+    const email = document.querySelector('input[name="email"]').value;
+    const mensagem = document.querySelector('textarea').value;
+    console.log(`Nome: ${nome}, E-mail: ${email}, Mensagem: ${mensagem}`);
+});
